d6b1f0af9e22a4fe53ffb7deaf45b32053083214
Patrick Boyd <patboyd@gmail.com>
2024-10-06 16:00:41 -0400

    format

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -3,16 +3,16 @@ import pg from 'pg'
 
 const { Client } = pg
 
-let client:pg.Client;
+let client: pg.Client;
 
-export async function getClient():Promise<pg.Client> {
+export async function getClient(): Promise<pg.Client> {
 
-    if(client) return client;
+    if (client) return client;
     client = new Client(env.POSTGRES_URL)
     await client.connect();
     return client
 }
 
-export async function cleanup():Promise<void> {
-    if(client) client.end();    
+export async function cleanup(): Promise<void> {
+    if (client) client.end();
 }
